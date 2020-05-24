018c70785368e21101f1d338c746db7773d6781c
archetype2142 <malhotraritwick2011@gmail.com>
2020-05-24 20:01:50 +0200

    Added mainview

---
+++ app/javascript/dashboard/MainView.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// import Navbar from "../global_components/Navbar";
+import SmoothScroll from 'smooth-scroll';
+
+class MainView extends Component {
+  constructor() {
+    super()
+  }
+  
+  componentDidMount() {
+    console.log("main view loaded");
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default MainView;
